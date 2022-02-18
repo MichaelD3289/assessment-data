9121fb22ecea683b7b1c368b0078f45893490f92
Michael Drummond <michael.drummond328@gmail.com>
2022-02-18 10:41:28 -0700

    finished with extra credit

--- process.py
+++ process.py
@@ -3,13 +3,24 @@
 log_file = open("um-server-01.txt")
 
 # here we are declaring a function that loops over each line in the file strips any white space and prints each line that has is on Tuesday. Looks like day is being set equal to the the characters found between index 0 and 3 to be tested.
-def sales_reports(log_file):
-    for line in log_file:
-        line = line.rstrip()
-        day = line[0:3]
-        # if day == "Tue":
-        if day == "Mon":
-            print(line)
+# def sales_reports(log_file):
+#     for line in log_file:
+#         line = line.rstrip()
+#         day = line[0:3]
+#         # if day == "Tue":
+#         if day == "Mon":
+#             print(line)
 
 # This is our function invoked with the file as the argument
-sales_reports(log_file)
+# sales_reports(log_file)
+
+# log_file.seek(0,0)
+
+def melon_orders(log_file):
+    for line in log_file:
+        if 'melon' in line:
+            split = line.rstrip().split(' ')
+            if int(split[2]) > 10:
+                 print(line)
+
+melon_orders(log_file)
